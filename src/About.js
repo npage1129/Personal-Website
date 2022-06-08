34475b433a63e9863fb310fa645aefa0b63e6b1e
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-06-08 06:51:48 -0600

    about me

--- src/About.js
+++ src/About.js
@@ -3,11 +3,23 @@ import './About.css'
 
 function About () {
   return (
+    <>
+    <div className="about">
+      <style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Raleway:wght@100&display=swap');
+</style>
+<div className="adjust">
+      <h2 className="heading">About Me</h2>
+      </div>
+      </div>
+      
+    
     <div className = "box">
     <img className = "fancy" src = "https://i.pinimg.com/564x/82/9b/61/829b617141460695390465398556676f.jpg"/>
    <img className= "me" src = "https://lh3.googleusercontent.com/Ga85hvyWgAUqGOsgTSbNc3IvmERVDlOWtT5cdNBAbDq7S8eT2OwCYJCuRD8DoG15dbiZFqB01Z1qKD3-yOv0LY_ADRFfEZvi8dxqt1X-3wh6CxCjH45mhrvY9sE0VEbEuFSHHS1D-uI=w2400"/>
    <img className = "fancy2" src = "https://i.pinimg.com/564x/82/9b/61/829b617141460695390465398556676f.jpg"/>
    </div>
+  </>
   )
 }
 export default About
